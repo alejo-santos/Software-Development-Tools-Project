cda0b3dee0164754a9a71c13adfd4b0ff1a4222c
jouna227 <159710378+jouna227@users.noreply.github.com>
2024-03-27 12:31:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import plotly.express as px
 # Read the dataset
 try:
     vehicles = pd.read_csv("/workspaces/Software-Development-Tools-Project/vehicles_us.csv")
-
 except FileNotFoundError:
     url = 'https://raw.githubusercontent.com/jouna227/Software-Development-Tools-Project/main/vehicles_us.csv'
     vehicles = pd.read_csv(url, index_col=0)
@@ -20,22 +19,21 @@ st.header('Dataset Overview')
 st.write(vehicles)
 
 # Filter by vehicle make
-selected_make = st.selectbox('Select a Vehicle Make', vehicles['model'].unique())
+selected_make = st.selectbox('Select a Vehicle Make', vehicles['make'].unique())
+filtered_vehicles = vehicles[vehicles['make'] == selected_make]
 
 # Histogram of Price
 st.header('Histogram of Price')
-fig_price = px.histogram(vehicles, x='price')
+fig_price = px.histogram(filtered_vehicles, x='price')
 st.plotly_chart(fig_price)
 
 # Scatter Plot of Price vs. Mileage
 st.header('Scatter Plot of Price vs. Mileage')
-fig_scatter = px.scatter(vehicles, x='odometer', y='price', title='Price vs. Mileage')
+fig_scatter = px.scatter(filtered_vehicles, x='odometer', y='price', title='Price vs. Mileage')
 st.plotly_chart(fig_scatter)
 
 # Checkbox to toggle logarithmic scale for price
 log_scale = st.checkbox('Use Logarithmic Scale for Price')
 if log_scale:
     fig_scatter.update_yaxes(type="log")
-
-
-
+    st.plotly_chart(fig_scatter)
