a8873a001a99b15d433e54f1de6258e6fe2cf16f
jouna227 <159710378+jouna227@users.noreply.github.com>
2024-03-26 20:05:42 -0400

    app.py

--- app.py
+++ app.py
@@ -22,22 +22,21 @@ st.write(vehicles)
 # Filter by vehicle make
 selected_make = st.selectbox('Select a Vehicle Make', vehicles['model'].unique())
 
-# Filtered dataset based on selected make
-filtered_vehicles = vehicles[vehicles['model'] == selected_make]
-
 # Histogram of Price
 st.header('Histogram of Models')
 
 # Check if filtered_vehicles DataFrame is not empty
 if not filtered_vehicles.empty:
-    fig_price = px.histogram(vehicles, x='price')
+    # Filtered dataset based on selected make
+    filtered_vehicles = vehicles[vehicles['price'] == selected_make]
+    fig_price = px.histogram(filtered_vehicles, x='price')
     st.plotly_chart(fig_price)
 else:
     st.write("No data available for the selected vehicle make.")
 
 # Scatter Plot of Price vs. Mileage
 st.header('Scatter Plot of Price vs. Mileage')
-fig_scatter = px.scatter(vehicles, x='odometer', y='price', title='Price vs. Mileage')
+fig_scatter = px.scatter(filtered_vehicles, x='odometer', y='price', title='Price vs. Mileage')
 st.plotly_chart(fig_scatter)
 
 # Checkbox to toggle logarithmic scale for price
